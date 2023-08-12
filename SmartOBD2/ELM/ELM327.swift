1eaaa604ac952a7ce50f208c590011d991ca9657
konteh <kkonteh97@gmail.com>
2023-08-12 09:05:27 -0700

    getting vin number and decoding it

--- SmartOBD2/ELM/ELM327.swift
+++ SmartOBD2/ELM/ELM327.swift
@@ -3,7 +3,7 @@
 //  SmartOBD2
 //
 //  Created by kemo konteh on 8/5/23.
-
+//
 
 import Foundation
 enum ELM327 {
@@ -244,8 +244,5 @@ enum ELM327 {
                 return .NONE
             }
         }
-        
-        
     }//END PROTOCOL
 }
-
--- SmartOBD2/Views/AppManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AppManager.swift
-//  SmartOBD2
-//
-//  Created by kemo konteh on 8/11/23.
-//
-
-import Foundation
-
