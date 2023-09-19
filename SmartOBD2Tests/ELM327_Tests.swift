195f7cb66c479002c3d796395ca4cbb338a9fda7
kkonteh97 <55326260+kkonteh97@users.noreply.github.com>
2023-09-19 15:11:15 -0700

    fix

---
+++ SmartOBD2Tests/ELM327_Tests.swift
@@ -0,0 +1,20 @@
+//
+//  ELM327_Tests.swift
+//  SmartOBD2Tests
+//
+//  Created by kemo konteh on 9/17/23.
+//
+
+import XCTest
+@testable import SmartOBD2
+
+// Naming Structure: test_UnitOfWork_StateUnderTest_ExpectedBehavior
+final class ELM327Tests: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
